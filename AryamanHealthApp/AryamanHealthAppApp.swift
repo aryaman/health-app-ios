82f33d81d337358fd1ceddcd773e819719c3aa2a
Aryaman <12789782+aryaman@users.noreply.github.com>
2025-06-07 01:06:18 -0400

    Added Initial Xcode Project

---
+++ AryamanHealthApp/AryamanHealthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AryamanHealthAppApp.swift
+//  AryamanHealthApp
+//
+//  Created by Aryaman Goel on 2025-06-07.
+//
+
+import SwiftUI
+
+@main
+struct AryamanHealthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
