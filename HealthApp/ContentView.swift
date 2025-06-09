b07788282e132ee0c29b55fcb867285077a5dea4
Aryaman <12789782+aryaman@users.noreply.github.com>
2025-06-08 22:23:02 -0400

    initial navigation setup

--- HealthApp/ContentView.swift
+++ HealthApp/ContentView.swift
@@ -8,42 +8,82 @@
 import SwiftUI
 import Charts
 
+enum HealthCategory: CaseIterable, Identifiable {
+    var id: Self { self }
+    
+    case steps
+    case weight
+    
+    var title: String {
+        switch self {
+        case .steps:
+            return "Steps"
+        case .weight:
+            return "Weight"
+        }
+    }
+    
+    var colorTint: Color {
+        switch self {
+        case .steps:
+            return .pink
+        case .weight:
+            return .indigo
+        }
+    }
+}
+
 struct ContentView: View {
+    
+    @State private var selectedCategory: HealthCategory = .steps
+    
     var body: some View {
         NavigationStack {
             ScrollView {
                 VStack(spacing: 20) {
-                    VStack {
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Label("Steps", systemImage: "figure.walk")
-                                    .font(.headline)
-                                    .foregroundStyle(.pink)
-                                Text("Avg: 10K Steps")
-                                    .font(.caption)
-                                    .foregroundStyle(.secondary)
-                            }
-                            
-                            Spacer()
-                            
-                            Image(systemName: "chevron.right")
-                                .foregroundStyle(.secondary)
+                    
+                    Picker("category", selection: $selectedCategory) {
+                        ForEach(HealthCategory.allCases) { category in
+                            Text(category.title)
                         }
-                        .padding(.bottom, 12)
-                        
-                        RoundedRectangle(cornerRadius: 12)
-                            .foregroundStyle(.secondary)
-                            .frame(height: 150)
-                        
                     }
-                    .padding()
-                    .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+                    .pickerStyle(.segmented)
+                    
+                    NavigationLink(value: selectedCategory) {
+                        VStack {
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Label(selectedCategory.title, systemImage: "figure.walk")
+                                        .font(.headline)
+                                        .foregroundStyle(selectedCategory.colorTint)
+                                    Text("Avg: 10K Steps")
+                                        .font(.caption)
+                                }
+                                
+                                Spacer()
+                                
+                                Image(systemName: "chevron.right")
+                            }
+                            .padding(.bottom, 12)
+                            
+                            Chart {
+                                LineMark(x: .value("yo", 0), y: .value("yo", 1))
+                                LineMark(x: .value("yo", 1), y: .value("yo", 3))
+                                LineMark(x: .value("yo", 2), y: .value("yo", 2))
+                                LineMark(x: .value("yo", 3), y: .value("yo", 4))
+                            }
+                            .frame(height: 150)
+                        }
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+                    }
+                    .foregroundStyle(.secondary)
                     
                     VStack(alignment: .leading) {
                         VStack(alignment: .leading) {
                             Label("Averages", systemImage: "calendar")
                                 .font(.headline)
-                                .foregroundStyle(.pink)
+                                .foregroundStyle(selectedCategory.colorTint)
                             Text("Last 28 Days")
                                 .font(.caption)
                                 .foregroundStyle(.secondary)
@@ -61,7 +101,11 @@ struct ContentView: View {
             }
             .padding()
             .navigationTitle("Dashboard")
+            .navigationDestination(for: HealthCategory.self) { category in
+                Text("Category: \(category.title)")
+            }
         }
+        .tint(selectedCategory.colorTint)
     }
 }
 
